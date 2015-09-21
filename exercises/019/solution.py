0c942a408bf1613bd97d19ff4a7d7aa88573030f
hkemble <kembleharry@gmail.com>
2015-09-21 14:29:31 +0200

    correction3

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -1,6 +1,5 @@
 import sys
 if len(sys.argv) > 1:
-    print(sum(sys.argv[1:])
+    print(sum(sys.argv[1:]))
 else:
-    print("usage: python3 solution.py OP1 OP2")   
-     
+    print("usage: python3 solution.py OP1 OP2")
