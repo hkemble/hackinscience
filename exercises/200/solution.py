6720a4ad55d8b92d395577bf8b33de6983dcf1df
hkemble <kembleharry@gmail.com>
2015-09-22 12:53:51 +0200

    cor1

---
+++ exercises/200/solution.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True  
+    elif n & 1 == 0:
+        return False
+    else:
+        for x in range(3, int(n**0.5)+1, 2):
+            if n % x == 0:
+                return False
+    return True
