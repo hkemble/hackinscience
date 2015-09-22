dbfc4e096bbefa7513822aa457f2d2b0e9139cb5
hkemble <kembleharry@gmail.com>
2015-09-22 10:10:38 +0200

    correction3

---
+++ exercises/107/solution.py
@@ -0,0 +1,12 @@
+def select_student(grades, cutoff):
+    import operator
+    accepted = []
+    rejected = []
+    for i in range(len(grades)):
+        if grades[i][2] >= cutoff:
+            accepted.append(grades[i])
+        else:
+            rejected.append(grades[i])
+    a_sort = sorted(accepted, key=operator.itemgetter(1), reverse=True)
+    r_sort = sorted(rejected, key=operator.itemgetter(1))
+    return {'Accepted': a_sort, 'Refused': r_sort}
