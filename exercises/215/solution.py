67597ded195f1c04a7286e15591a1fa68758d874
hkemble <kembleharry@gmail.com>
2015-09-22 13:54:01 +0200

    cor1

---
+++ exercises/215/solution.py
@@ -0,0 +1,11 @@
+import prime_tools
+
+
+track = []
+for i in range(222281, 222381):
+    binar = str(bin(i)[2:])
+    for j in range(len(binar)):
+        if binar[j] == 1:
+            track.append(1)
+    if prime_tools.is_prime(len(track)):
+        print(i)
