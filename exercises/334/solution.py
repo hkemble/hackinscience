69c91de003897df0da0d4c0ee21e7259b65dfb4c
hkemble <kembleharry@gmail.com>
2015-09-23 17:52:33 +0200

    cs

--- exercises/334/solution.py
+++ exercises/334/solution.py
@@ -5,12 +5,12 @@ with open('velib_clean.json', 'r') as json_data:
     data = json.load(json_data)
 
 
-def locate(tup):
+def locate(lat, long):
     closest = 1000000000
     city = 'none'
     for i in range(len(data)):
-        new = (((data[i]['latitude'] - tup[0]) ** 2 +
-                (data[i]['longitude'] - tup[1]) ** 2) ** 0.5)
+        new = (((data[i]['latitude'] - lat) ** 2 +
+                (data[i]['longitude'] - long) ** 2) ** 0.5)
         if new < closest:
             closest = new
             city = data[i]['name']
