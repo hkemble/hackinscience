6eeb7523ee4d52988483faccfa1d7ffbc71ca279
hkemble <kembleharry@gmail.com>
2015-09-23 17:11:41 +0200

    cs

---
+++ exercises/334/solution.py
@@ -0,0 +1,17 @@
+import json
+
+
+with open('velib_clean.json', 'r') as json_data:
+    data = json.load(json_data)
+
+
+def locate(lat, long):
+    closest = 1000000000
+    city = 'none'
+    for i in range(len(data)):
+        new = (((data[i]['latitude'] - lat) ** 2 +
+                (data[i]['longitude'] - long) ** 2) ** 0.5)
+        if new < closest:
+            closest = new
+            city = data[i]['city']
+    return {'distance': closest, 'city': city}
