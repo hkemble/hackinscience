5b8cdf3aada85f28dc4808e9b1d941a4eeae697c
hkemble <kembleharry@gmail.com>
2015-09-25 00:17:14 +0200

    correction

--- exercises/350/solution.py
+++ exercises/350/solution.py
@@ -1,20 +1,5 @@
 import bencode_tools as bt
 
 
-def encode2(obj):
-    if type(obj) == bytes or int:
-        return bt.bencode_dic[str(type(obj))](obj)
-    elif type(obj) == dict:
-        for i in obj:
-            obj[i] = bt.bencode_dic[str(type(obj[i]))](obj)[i]
-        return obj
-    elif type(obj) == list:
-        for i in range(len(list)):
-            obj[i] = bt.bencode_dic[str(type(obj[i]))](obj)[i]
-        return obj
-    else:
-        return obj
-
-
 def encode(stuff):
-    return bytes(encode2(stuff), encoding='ascii')
+    return bytes(bt.encode2(stuff), encoding='ascii')
